0890415ec8108d0948894ab6636bd7a53e52dd4f
Alex <alex@gmail.com>
2021-05-09 08:35:56 +0200

    Updates

---
+++ precache-manifest.d5eb4644eef394d7e04e7d4123de4c13.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835773f357823f8d4b80e70708649cbc",
+    "url": "/AlexBen92/commerce2.git/index.html"
+  },
+  {
+    "revision": "0233ec15b97c2e5ff1fc",
+    "url": "/AlexBen92/commerce2.git/static/css/2.cb4b99d6.chunk.css"
+  },
+  {
+    "revision": "1799d05766e365674f42",
+    "url": "/AlexBen92/commerce2.git/static/css/main.3a673bd1.chunk.css"
+  },
+  {
+    "revision": "0233ec15b97c2e5ff1fc",
+    "url": "/AlexBen92/commerce2.git/static/js/2.95f52fe4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AlexBen92/commerce2.git/static/js/2.95f52fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1799d05766e365674f42",
+    "url": "/AlexBen92/commerce2.git/static/js/main.f30f407b.chunk.js"
+  },
+  {
+    "revision": "d4bf6d86c68fdc3b1a1d",
+    "url": "/AlexBen92/commerce2.git/static/js/runtime-main.d7a530e3.js"
+  },
+  {
+    "revision": "0210e597f574b4bd3f751ca6b3629f1c",
+    "url": "/AlexBen92/commerce2.git/static/media/bbb.0210e597.jpg"
+  },
+  {
+    "revision": "59731bae1af505b797e0bab856268623",
+    "url": "/AlexBen92/commerce2.git/static/media/dslr.59731bae.jpg"
+  },
+  {
+    "revision": "8d318e4a591da8b33dddbe1467a809c9",
+    "url": "/AlexBen92/commerce2.git/static/media/iphone.8d318e4a.jpg"
+  },
+  {
+    "revision": "e7bdef88d0ff67525ebeb96877270e62",
+    "url": "/AlexBen92/commerce2.git/static/media/microphone.e7bdef88.jpg"
+  },
+  {
+    "revision": "92ef6ab7b31320d776128b8bf7944473",
+    "url": "/AlexBen92/commerce2.git/static/media/perfume.92ef6ab7.jpg"
+  },
+  {
+    "revision": "986af3717a208f0b993fffa0d353abe6",
+    "url": "/AlexBen92/commerce2.git/static/media/rings.986af371.jpg"
+  },
+  {
+    "revision": "f30fe0b2e59c7281ecf5bd07d202a595",
+    "url": "/AlexBen92/commerce2.git/static/media/shoes.f30fe0b2.jpg"
+  },
+  {
+    "revision": "b4e58e91e391491d2836ce808e677037",
+    "url": "/AlexBen92/commerce2.git/static/media/watch.b4e58e91.jpg"
+  }
+]);
